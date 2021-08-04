1cc7631868edbc32c9e02eed85f1e682904685b5
your name hereEddy_Marshall <epmarsha2@gmail.com>
2021-08-04 14:24:26 -0400

    finish piece and start board

---
+++ reversi/src/piece.js
@@ -0,0 +1,46 @@
+/**
+ * Initializes the Piece with its color.
+ */
+function Piece (color) {
+    this.color = color
+}
+
+/**
+ * Returns the color opposite the current piece.
+ */
+Piece.prototype.oppColor = function () {
+
+    // let color = ""
+    if (this.color === "black") {
+        return "white"
+    } else {
+        return "black"
+    }
+    // const opp = new Piece(color)
+    // return color
+};
+
+/**
+ * Changes the piece's color to the opposite color.
+ */
+Piece.prototype.flip = function () {
+    this.color = this.oppColor()
+};
+
+/**
+ * Returns a string representation of the string
+ * based on its color.
+ */
+Piece.prototype.toString = function () {
+    if (this.color === "black") {
+        return "B"
+    } else {
+        return "W"
+    }
+};
+
+// DON'T TOUCH THIS CODE
+if (typeof window === 'undefined'){
+    module.exports = Piece;
+}
+// DON'T TOUCH THIS CODE
